4ae789327a5f9ecf3ba0f6efe012eb73bae39606
susanahmed <susanahmed617@gmail.com>
2023-01-14 00:35:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = "Susan is the champion";
+document.body.append(newHeader);
+
+//document.getElementById('victory').textContent = 'SUSAN is the champion';
+//newHeader.textContent = `Susan is the champion`;
+
+
+
